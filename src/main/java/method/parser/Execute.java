7102c90d76dc4151c25eaf42c3fae51128dd1ed6
Syed Ishtiaque Ahmad <ishtiaque05@gmail.com>
2020-05-26 15:57:02 -0700

    Parsing with JavaParser

---
+++ src/main/java/method/parser/Execute.java
@@ -0,0 +1,42 @@
+package method.parser;
+import com.github.javaparser.StaticJavaParser;
+import com.github.javaparser.ast.body.MethodDeclaration;
+import com.github.javaparser.ast.expr.MethodCallExpr;
+import com.github.javaparser.ast.visitor.VoidVisitorAdapter;
+import com.github.javaparser.resolution.MethodUsage;
+import com.github.javaparser.resolution.declarations.ResolvedMethodDeclaration;
+import com.github.javaparser.symbolsolver.javaparsermodel.JavaParserFacade;
+import com.github.javaparser.symbolsolver.model.resolution.SymbolReference;
+import com.github.javaparser.symbolsolver.model.resolution.TypeSolver;
+import com.github.javaparser.symbolsolver.resolution.typesolvers.CombinedTypeSolver;
+import com.google.common.base.Strings;
+import method.parser.DirExplorer;
+
+import java.io.File;
+import java.io.IOException;
+
+public class Execute {
+    public static void listMethodCalls(File projectDir) {
+        new DirExplorer((level, path, file) -> path.endsWith(".java"), (level, path, file) -> {
+            System.out.println(path);
+            System.out.println(Strings.repeat("=", path.length()));
+            try {
+                new VoidVisitorAdapter<Object>() {
+                    @Override
+                    public void visit(MethodCallExpr n, Object arg) {
+                        super.visit(n, arg);
+                        System.out.println(" [L " + n.getBegin().get().line + "] " + n);
+                    }
+                }.visit(StaticJavaParser.parse(file), null);
+                System.out.println(); // empty line
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        }).explore(projectDir);
+    }
+
+    public static void main(String[] args) {
+        File projectDir = new File("/home/ishtiaque/Desktop/projects/examples");
+        listMethodCalls(projectDir);
+    }
+}
