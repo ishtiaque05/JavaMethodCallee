a125468aebc7b704ee1be6c246817873a39eda47
Syed Ishtiaque Ahmad <ishtiaque05@gmail.com>
2020-05-26 19:36:45 -0700

    Get method caller list using JavaParser

--- src/main/java/method/parser/Execute.java
+++ src/main/java/method/parser/Execute.java
@@ -14,6 +14,7 @@ import method.parser.DirExplorer;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.List;
 
 public class Execute {
     public static void listMethodCalls(File projectDir) {
@@ -22,10 +23,42 @@ public class Execute {
             System.out.println(Strings.repeat("=", path.length()));
             try {
                 new VoidVisitorAdapter<Object>() {
+//                    @Override
+//                    public void visit(MethodCallExpr n, Object arg) {
+//                        super.visit(n, arg);
+//                        System.out.println(" [L " + n.getBegin().get().line + "] " + n);
+//                    }
+
+                    @Override
+                    public void visit(MethodDeclaration aMethod, Object arg) {
+                        System.out.println(aMethod.getDeclarationAsString(true, false));
+                        System.out.println("{");
+                        String declaredMethod = aMethod.getNameAsString();
+                        List<MethodCallExpr> callExprList = aMethod.findAll(MethodCallExpr.class);
+                        for (MethodCallExpr callExpr : callExprList) {
+                            String calledMethodName = callExpr.getName().getIdentifier();
+                            System.out.println(calledMethodName);
+//                            visitMethodCall(callExpr, arg, declaredMethod);
+//                            CombinedTypeSolver solver = typeSolver(srcFolders);
+//                            JavaParserFacade j = JavaParserFacade.get(solver);
+//                            SymbolReference<ResolvedMethodDeclaration> methodRef = j.solve(callExpr);
+//                            String methodSignature = methodRef.getCorrespondingDeclaration().getQualifiedSignature();
+//                            if(methodSignature.contains("javax.security.auth") || methodSignature.contains("javax") || methodSignature.contains("security") || methodSignature.contains("auth")) {
+//                                System.out.println(methodSignature);
+//                            }
+                        }
+                        System.out.println("}\n");
+                        super.visit(aMethod, arg);
+
+                    }
+
+                    public void visitMethodCall(MethodCallExpr n, Object arg, String declaredMethod) {
+                        super.visit(n, arg);
+                    }
+
                     @Override
                     public void visit(MethodCallExpr n, Object arg) {
                         super.visit(n, arg);
-                        System.out.println(" [L " + n.getBegin().get().line + "] " + n);
                     }
                 }.visit(StaticJavaParser.parse(file), null);
                 System.out.println(); // empty line
