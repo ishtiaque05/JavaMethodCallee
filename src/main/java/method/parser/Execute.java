1a85f06d2d29e2fddb06a5f8f70d0009aba817b0
Syed Ishtiaque Ahmad <ishtiaque05@gmail.com>
2020-06-18 09:15:42 -0700

    Count solved and unsolved errors and save exceptions messages

--- src/main/java/method/parser/Execute.java
+++ src/main/java/method/parser/Execute.java
@@ -1,30 +1,28 @@
 package method.parser;
-import com.github.javaparser.JavaParser;
-import com.github.javaparser.ParseResult;
-import com.github.javaparser.ParserConfiguration;
+
 import com.github.javaparser.StaticJavaParser;
-import com.github.javaparser.ast.CompilationUnit;
 import com.github.javaparser.ast.body.MethodDeclaration;
 import com.github.javaparser.ast.expr.MethodCallExpr;
 import com.github.javaparser.ast.visitor.VoidVisitorAdapter;
-import com.github.javaparser.resolution.MethodUsage;
 import com.github.javaparser.resolution.UnsolvedSymbolException;
-import com.github.javaparser.resolution.declarations.ResolvedMethodDeclaration;
 import com.github.javaparser.symbolsolver.JavaSymbolSolver;
-import com.github.javaparser.symbolsolver.javaparsermodel.JavaParserFacade;
-import com.github.javaparser.symbolsolver.model.resolution.SymbolReference;
 import com.github.javaparser.symbolsolver.model.resolution.TypeSolver;
 import com.github.javaparser.symbolsolver.resolution.typesolvers.CombinedTypeSolver;
 import com.github.javaparser.symbolsolver.resolution.typesolvers.JavaParserTypeSolver;
 import com.github.javaparser.symbolsolver.resolution.typesolvers.ReflectionTypeSolver;
 import com.google.common.base.Strings;
-import method.parser.DirExplorer;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 
 public class Execute {
+    public static int errors = 0;
+    public static int solved = 0;
+    public static int unsolved = 0;
+    public static int jUnitUnsolved = 0;
+    public static List<String> errsMsg = new ArrayList<String>();;
     public static void listMethodCalls(File projectDir) {
         new DirExplorer((level, path, file) -> path.endsWith(".java"), (level, path, file) -> {
             try {
@@ -44,17 +42,17 @@ public class Execute {
                     @Override
                     public void visit(MethodDeclaration aMethod, Object arg) {
                         System.out.println(aMethod.getDeclarationAsString(true, false));
-                        System.out.println();
-                        System.out.println("{");
-                        String declaredMethod = aMethod.getNameAsString();
+                        System.out.println(aMethod.getNameAsString());
                         List<MethodCallExpr> callExprList = aMethod.findAll(MethodCallExpr.class);
                         for (MethodCallExpr callExpr : callExprList) {
 //                          Getting corresponding method declaration
 //                          JavaParserFacade.get(new JavaParserTypeSolver("/home/ishtiaque/Desktop/projects/testCallGraph/src/main/java")).solve(callExpr);
+                            System.out.println(callExpr.getName());
                             callExpr.resolve().toAst();
                             System.out.println(callExpr.resolve().getQualifiedName());
                             System.out.println(callExpr.resolve().toAst().get().getRange().get().begin.line);
                             System.out.println(callExpr.resolve().toAst().get().getParentNode().get().findCompilationUnit().get().getStorage().get().getPath());
+                            Execute.solved++;
                         }
                         System.out.println("}\n");
                         super.visit(aMethod, arg);
@@ -63,41 +61,42 @@ public class Execute {
                 }.visit(StaticJavaParser.parse(file), null);
                 System.out.println(); // empty line
             } catch (UnsolvedSymbolException usym) {
-                System.out.println(usym);
+                if(usym.getName().contains("junit")) {
+                    Execute.jUnitUnsolved++;
+                } else {
+                    Execute.unsolved++;
+                }
+                System.out.println("Unsolved Exception:"+ usym);
             }
             catch (Exception e) {
-                System.out.println(e);
+                Execute.errors++;
+                System.out.println(e.getMessage());
+                Execute.errsMsg.add(e.getMessage());
             }
         }).explore(projectDir);
     }
 
     public static void main(String[] args) {
-        File projectDir = new File("/home/ishtiaque/Desktop/projects/JavaMethodCallee/testExamples/testCallGraph/src/test");
-        File srcDir = new File("/home/ishtiaque/Desktop/projects/JavaMethodCallee/testExamples/testCallGraph/src");
+        File testDir = new File("/home/ishtiaque/Desktop/projects/Research/commons-lang/src/test");
+        File srcDir = new File("/home/ishtiaque/Desktop/projects/Research/commons-lang/src");
         TypeSolver myTypeSolver;
         CombinedTypeSolver cbSolver = new CombinedTypeSolver(new JavaParserTypeSolver(srcDir), new ReflectionTypeSolver());
         if(srcDir.exists() && srcDir.isDirectory()){
             File fileArr[] = srcDir.listFiles();
             myTypeSolver = addAllSrcPath(fileArr, cbSolver);
-//            System.out.println(myTypeSolver);
         } else {
             myTypeSolver = cbSolver;
         }
-//            TypeSolver myTypeSolver = new CombinedTypeSolver(
-//                new ReflectionTypeSolver(),
-//                new JavaParserTypeSolver(new File("/home/ishtiaque/Desktop/projects/JavaMethodCallee/testExamples/testCallGraph/src/main/java/demo/nested")),
-//                new JavaParserTypeSolver(new File("/home/ishtiaque/Desktop/projects/JavaMethodCallee/testExamples/testCallGraph/src/main/java/demo"))
-//        );
-//        TypeSolver typeSolver = new ReflectionTypeSolver();
         JavaSymbolSolver symbolSolver = new JavaSymbolSolver(myTypeSolver);
         StaticJavaParser.getConfiguration().setSymbolResolver(symbolSolver);
-        listMethodCalls(projectDir);
+        listMethodCalls(testDir);
+        System.out.println("Solved: "+Execute.solved+ " Unsolved Assertions:"+ Execute.jUnitUnsolved + " Unsolved without Junit:" +Execute.unsolved + " Errors: "+ Execute.errors);
+        System.out.println(Execute.errsMsg);
     }
 
     public static TypeSolver addAllSrcPath(File[] files, CombinedTypeSolver myTypeSolver) {
         for(File f: files) {
             if(f.isDirectory() && !f.isFile() && !f.getAbsolutePath().contains("src/test")) {
-//                System.out.println(f.getAbsolutePath());
                 myTypeSolver.add(new JavaParserTypeSolver(new File(f.getAbsolutePath())));
                 addAllSrcPath(f.listFiles(), myTypeSolver);
             }
--- testExamples/testCallGraph/src/test/java/CalculatorTest.java
+++ testExamples/testCallGraph/src/test/java/CalculatorTest.java
@@ -2,6 +2,9 @@ import demo.Calculator;
 import demo.*;
 import junit.framework.Assert;
 import org.junit.Test;
+import java.io.File;
+import java.io.IOException;
+import java.util.List;
 
 public class CalculatorTest {
 
@@ -15,6 +18,14 @@ public class CalculatorTest {
         int result = calculator.add(2, 2);
         int result2 =  c.add(4,4);
         int result3 = ec.add(5,5);
+        File srcDir = new File("/home/ishtiaque/Desktop/projects/JavaMethodCallee/testExamples/testCallGraph/src");
+        if (srcDir.exists()) {
+            assertTrue(srcDir.exists());
+            srcDir.listFiles();
+        }
+        assertEquals(0,0);
+        assertEquals(1,1);
+
         System.out.println(result3);
         // Then
         if (result != 4) {   // if 2 + 2 != 4
