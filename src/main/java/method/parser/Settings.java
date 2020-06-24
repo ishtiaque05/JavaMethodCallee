d09e7ac75d1f4ff4e64114119e313e0ae2097edb
Syed Ishtiaque Ahmad <ishtiaque05@gmail.com>
2020-06-23 22:57:46 -0700

    compiled for beluga server

--- src/main/java/method/parser/Settings.java
+++ src/main/java/method/parser/Settings.java
@@ -1,6 +1,6 @@
 package method.parser;
 
 public class Settings {
-    final static String OUTPATH = "/home/ishtiaque/Desktop/projects/JavaMethodCallee/data/";
-    final static String REPOS_PATH = "/home/ishtiaque/Desktop/projects/Research/";
+    final static String OUTPATH = "/home/siahmad/projects/def-rtholmes-ab/siahmad/processedData/testMethodCallGraph/";
+    final static String REPOS_PATH = "/home/siahmad/projects/def-rtholmes-ab/siahmad/repos/";
 }
