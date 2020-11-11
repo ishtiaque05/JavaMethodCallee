18139e61b0cabc9489031bcc472ed0df9a0c105d
Syed Ishtiaque Ahmad <ishtiaque05@gmail.com>
2020-11-11 00:36:23 -0800

    Remove absolute file path from path

--- src/main/java/method/parser/JSONFormatterHelper.java
+++ src/main/java/method/parser/JSONFormatterHelper.java
@@ -6,16 +6,18 @@ import com.github.javaparser.resolution.declarations.ResolvedMethodDeclaration;
 
 public class JSONFormatterHelper {
     public static TestMethodInfo getInfoModel(MethodDeclaration aMethod) {
+        String repo_path = Settings.REPOS_PATH + Settings.REPO + "/";
         TestMethodInfo tmethod = new TestMethodInfo();
         tmethod.name = aMethod.getNameAsString();
         tmethod.methodSignature = aMethod.getSignature().asString();
         tmethod.startline = aMethod.getName().getBegin().get().line;
         tmethod.endline = aMethod.getEnd().get().line;
-        tmethod.path = aMethod.findCompilationUnit().get().getStorage().get().getPath().toString();
+        tmethod.path = aMethod.findCompilationUnit().get().getStorage().get().getPath().toString().replace(repo_path, "");
         return tmethod;
     }
 
     public static CalledMethodInfo getCalledMethodModel(MethodCallExpr callExpr, ResolvedMethodDeclaration resolvedMethod) {
+        String repo_path = Settings.REPOS_PATH + Settings.REPO + "/";
         CalledMethodInfo cMethod = new CalledMethodInfo();
         cMethod.name = callExpr.getNameAsString();
         cMethod.className = resolvedMethod.getClassName();
@@ -24,7 +26,7 @@ public class JSONFormatterHelper {
         cMethod.signature = resolvedMethod.getSignature();
         cMethod.startline = resolvedMethod.toAst().get().getName().getBegin().get().line;
         cMethod.endline = resolvedMethod.toAst().get().getName().getEnd().get().line;
-        cMethod.path = resolvedMethod.toAst().get().findCompilationUnit().get().getStorage().get().getPath().toString();
+        cMethod.path = resolvedMethod.toAst().get().findCompilationUnit().get().getStorage().get().getPath().toString().replace(repo_path, "");
         return cMethod;
     }
 }
